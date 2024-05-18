ec0ced844f1178df7c3f83e7e00db627c289a6c1
Rellikeht <michaelhemperek@gmail.com>
2024-05-18 12:31:03 +0200

    quickfix

--- server/user.nix
+++ server/user.nix
@@ -18,7 +18,6 @@
   normalPackages = with pkgs; ([
       pkgtop
       luaformatter
-      luacheck
       # # It is sometimes useful to fine-tune packages, for example, by applying
       # # overrides. You can do that directly here, just don't forget the
       # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
