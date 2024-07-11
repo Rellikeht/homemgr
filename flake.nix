af9a4b8cdde800b2f20e5c5b6aa6a37133b06874
Rellikeht <michaelhemperek@gmail.com>
2024-07-11 17:56:25 +0200

    --amend

--- flake.nix
+++ flake.nix
@@ -218,7 +218,7 @@
             })
             // (homeConfs {
               name = serverName;
-              inherit mmods;
+              mods = mmods;
               confName = "minecraftServer";
             })
             // {
